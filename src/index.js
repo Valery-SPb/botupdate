c1926f4a0810215fb60309bdea1fe66b2141837f
Valery_SPb <russianline@gmail.com>
2022-10-29 15:24:47 +0300

    HW09 - Remove class (Hidden)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const mainClass = document.querySelector(".main-class");
 function setDetails(anchor) {
 	detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
 	detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
 }
-for(let i = 0; anchors.length; i++) {
+function showDetails() {
+	mainClass.classList.remove("hidden")
+}
+for(let i = 0; i < anchors.length; i++) {
 	anchors[i].addEventListener("click", function() {
 	console.log("Event - click on", anchors[i]);
+	showDetails();
 	setDetails(anchors[i]);
 	})
 }
