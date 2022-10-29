3998445c8474e5619a2831bfc88a3f164dcc7d20
Valery_SPb <russianline@gmail.com>
2022-10-29 16:58:41 +0300

    HW09-Close Button. Selector for two classes.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden";
 function setDetails(anchor) {
 	detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
 	detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
 }
-function showDetails() {
-	mainClass.classList.remove("hidden")
-}
 for(let i = 0; i < anchors.length; i++) {
 	anchors[i].addEventListener("click", function() {
-	console.log("Event - click on", anchors[i]);
-	showDetails();
-	setDetails(anchors[i]);
+		console.log("Event - click on", anchors[i]);
+		showDetails();
+		setDetails(anchors[i]);
 	})
+}function showDetails() {
+	mainClass.classList.remove(HIDDEN)
+}
+function hideDetails() {
+	mainClass.classList.add(HIDDEN)
 }
