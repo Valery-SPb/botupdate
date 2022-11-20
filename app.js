79dcfc0c6ed2e0b6ca790d692359560677587cce
Your Name <russianlineQgmail.com>
2022-11-20 18:15:04 +0300

    HW13-02 generateRandomTeudatZehut

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-
 let teudatStrNumber ="123456782";
 console.log(checkTeudatZehut(teudatStrNumber));
+console.log(generateRandomTeudatZehut());
+console.log(checkTeudatZehut(generateRandomTeudatZehut()));
 
 function checkTeudatZehut(teudatStrNumber) {
-	let inStr = Array.from(teudatStrNumber);
-	if (inStr.length != 9) {
+	let inArr = Array.from(teudatStrNumber);
+	if (inArr.length != 9) {
 		return "not 9 numbers"
 	}
-	let resStr =inStr.map(function(symbol, index) {
+	return resSumm(inArr) % 10 == 0;
+}
+	function resSumm(inArr) {
+	let resArr = inArr.map(function(symbol, index) {
 		return index % 2 == 0 ? +symbol : +oddSymbol(symbol);
 	});
-	let res = resStr.reduce(function(res, cur) {
+	return resArr.reduce(function(res, cur) {
 		return res + cur;
 	},0);
-	res = res % 10 == 0 ? "true" : "false";
-	return res;
 }
-
 function oddSymbol(symbol) {
 	return symbol * 2 < 10 ? symbol * 2 : 1 + (symbol * 2) % 10;
 }
+function generateRandomTeudatZehut() {
+	let TeudatArr = []; 
+	for (let i = 0; i < 8; i++) {
+		TeudatArr[i] = Math.round(Math.random() * 9);
+	}
+	TeudatArr [8] = 10 - resSumm(TeudatArr) % 10;
+	return TeudatArr.join("");
+}
+
