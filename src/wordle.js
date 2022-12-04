46e416faa89ca38374a13c1d15d5cfe03ee271fe
Valery_SPb <russianline@gmail.com>
2022-12-04 16:10:28 +0300

    HW16 - wordle

--- src/wordle.js
+++ src/wordle.js
@@ -1,9 +1,17 @@
 const word = "table";
 const N_LETTERS = 5;
-const letterElements = document.querySelectorAll(".letter-guess")
+const letterElements = document.querySelectorAll(".letter-guess");
+const Annotation = document.querySelector(".ant");
+var numMove = 5;
 function onChange(event) {
 	const wordGuess = event.target.value;
-	event.target.value ='';
+	//event.target.value ='';
+	
+	if(word == wordGuess) {
+		Annotation.innerHTML = "Congratulations you have guessed word";
+		return;
+	}
+
 	if (wordGuess.length != N_LETTERS) {
 		alert(`Word should contain ${N_LETTERS} letters`)
 	} else {
@@ -17,8 +25,17 @@ function onChange(event) {
 			}
 			return res;
 		})
+		numMove = numMove - 1;
+		backText ();
 		colors.forEach((c,i) => letterElements[i].style.color=c);
 	}
-	alert()
+}
+
+function backText () {
+	if(numMove > 0) {
+		Annotation.innerHTML = `Now you have only ${numMove} moves`;
+	} else {
+		Annotation.innerHTML = "Sorry your guess trials have ended up";
+	}
 
 }
